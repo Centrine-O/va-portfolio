055f3fc4d1cca5eaaaeb328eca05691719547850
CENTRINE ONG'ARIA <133117006+Centrine-O@users.noreply.github.com>
2025-09-29 12:15:28 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base = '/va-portfolio/',
+  base:'/va-portfolio/',
   server: {
     host: "::",
     port: 8080,
